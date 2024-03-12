70ea898f1b43f67e11ed7b39b90893a4caba31e9
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-03-12 10:20:55 -0700

    local.properties autogenerated in build.bat (guess work)

--- build.bat
+++ build.bat
@@ -1,5 +1,59 @@
 @echo off
 
+:: Java JDR/JRE is expected here:
+:: C:\Program Files\Android\Android Studio\jbr\bin
+:: C:\Program Files\Java\jdk-21\bin
+
+if not defined JAVA_HOME (
+    if exist "%ProgramFiles%\Android\Android Studio\jbr\bin\javac.exe" (
+        set JAVA_HOME="%ProgramFiles%\Android\Android Studio\jbr"
+	)
+)
+
+if not defined JAVA_HOME (
+    if exist "%ProgramFiles%\jdk-21\bin\javac.exe" (
+        set JAVA_HOME="%ProgramFiles%\\jdk-21"
+	)
+)
+
+:: TODO: may be remove it?
+if not defined JAVA_HOME (
+    if exist "%ProgramFiles%\jdk-20\bin\javac.exe" (
+        set JAVA_HOME="%ProgramFiles%\\jdk-20"
+	)
+)
+
+if defined JAVA_HOME goto java_home_ok
+echo "Install JDK-21 and/or Android Studio or Android SDK"
+echo "https://www.oracle.com/java/technologies/downloads/#jdk21-windows"
+echo "or https://jdk.java.net/21/"
+exit /b 1
+:java_home_ok
+echo JAVA_HOME=%JAVA_HOME%
+
+:: Assumption Android Studio is installed for current user only:
+if not defined ANDROID_HOME (
+	if exist "%USERPROFILE%\AppData\Local\Android\Sdk" (
+        set ANDROID_HOME="%USERPROFILE%\AppData\Local\Android\Sdk"
+	)
+)
+
+if defined ANDROID_HOME goto android_home_ok
+echo Install Android Studio or Android SDK
+exit /b 1
+:android_home_ok
+echo ANDROID_HOME=%ANDROID_HOME%
+
+:: change next line to reflect where Android SDK is installed.
+:: IMPORTANT: both ":" and "\" must be escaped with "\"
+if not exist local.properties (
+    set ANDROID_HOME_ESCAPED=%ANDROID_HOME:\=\\:%
+    set ANDROID_HOME_ESCAPED=!ANDROID_HOME_ESCAPED:/=\\!
+    > local.properties echo sdk.dir=!ANDROID_HOME_ESCAPED!
+)
+
+::  > local.properties echo sdk.dir=C\:\\Users\\%username%\\AppData\\Local\\Android\\Sdk
+
 :: Could not compile settings file settings.gradle
 :: Unsupported class file major version 65
 ::
@@ -31,10 +85,10 @@
 :: see:
 :: https://services.gradle.org/distributions/
 
-:: **Mimic project array declaration (no true arrays in batch)**
+:: **project array declaration (no true arrays in batch)**
 set projects=native-activity other-builds\ndkbuild\bitmap-plasma other-builds\ndkbuild\gles3jni other-builds\ndkbuild\hello-gl2
 
-:: **Mimic array of APK paths**
+:: **array of APK paths**
 set apks=native-activity\app\build\outputs\apk\debug\app-debug.apk other-builds\ndkbuild\bitmap-plasma\app\build\outputs\apk\debug\app-debug.apk other-builds\ndkbuild\gles3jni\app\build\outputs\apk\debug\app-debug.apk other-builds\ndkbuild\hello-gl2\app\build\outputs\apk\debug\app-debug.apk
 
 :: **Set temp file names**
@@ -49,17 +103,21 @@ if defined RUNNER_DEBUG (
     set VERBOSITY=--quiet
 )
 
+:: Copy local.properties to each project directory
+for %%d in (%projects%) do (
+    if not exist "%%dd\local.properties" (
+	    @copy /Y local.properties %%d > nul
+	)
+)
+
 :: **Iteration and building logic**
 for %%d in (%projects%) do (
     pushd %%d >nul
     echo Building %%d
-
-    :: Note: TERM=dumb might not have a direct equivalent
     if errorlevel 1 (
        set SAMPLE_CI_RESULT=1
        echo %%d >> %LINT_FAILURES% 
     )
-
     call gradlew %VERBOSITY% assembleDebug
     if errorlevel 1 (
        set SAMPLE_CI_RESULT=1
@@ -100,4 +158,5 @@ if not exist %LINT_FAILURES% if not exist %BUILD_FAILURES% if not exist %APK_FAI
     echo 
     echo ======= BUILD SUCCESS ======
 )
-del %LINT_FAILURES% %BUILD_FAILURES% %APK_FAILURES% 
+del %LINT_FAILURES% %BUILD_FAILURES% %APK_FAILURES%
+
